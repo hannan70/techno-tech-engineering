80d5fc1beeb06a7082d0a3878b9ed4909d273d08
hannan70 <hannan728070@gmail.com>
2026-01-28 12:28:36 +0600

    category done

--- backend/categories/models.py
+++ backend/categories/models.py
@@ -1,5 +1,25 @@
 from django.db import models
+from PIL import Image
+
+# def category_image_size(image):
+#     img = Image.open(image)
+#     width, height = img.size
+    
+    # REQUIRED_WIDTH =
+    # REQUIRED_HEIGHT = 
+
 
 # Create your models here.
 class Category(models.Model):
-    image      = models.ImageField(upload_to="photos")
+    image         = models.ImageField(upload_to="photos")
+    category_name = models.CharField(max_length=255, unique=True)
+    qty           = models.PositiveIntegerField(default=0)
+    
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        ordering = ['category_name']
+    
+    
+    def __str__(self):
+        return self.category_name
--- backend/pages/views.py
+++ backend/pages/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from banners.models import Banner
 from site_settings.models import SiteSettings
+from categories.models import Category
 
 # Create your views here.
 def home_page(request):
     banner = Banner.objects.order_by("-id").first()
     site_settings = SiteSettings.objects.first()
-    return render(request, "base.html", context={"banner": banner, "site_settings": site_settings})
+    categories = Category.objects.all()
+    context = {
+        "banner": banner, 
+        "site_settings": site_settings,
+        "categories" : categories
+    }
+    return render(request, "index.html", context)
